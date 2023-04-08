f495ac0bd927476513a8ec1ef8e69b1048562802
grey <1186463245@qq.com>
2023-04-09 00:29:27 +0800

    lab2

--- src/ethernet.c
+++ src/ethernet.c
@@ -11,6 +11,16 @@
 void ethernet_in(buf_t *buf)
 {
     // TO-DO
+    // 首先判断数据长度，如果数据长度小于以太网头部长度，则认为数据包不完整，丢弃不处理
+    if (buf->len < sizeof(ether_hdr_t))
+        return;
+    // 获取以太网包头数据并移除以太网包头
+    ether_hdr_t *hdr = (ether_hdr_t *)buf->data;
+    uint8_t *src = hdr->src;
+    uint16_t protocol = swap16(hdr->protocol16);
+    buf_remove_header(buf, sizeof(ether_hdr_t));
+    // 调用net_in()函数向上层传递数据包
+    net_in(buf, protocol, src);
 }
 /**
  * @brief 处理一个要发送的数据包
@@ -28,13 +38,12 @@ void ethernet_out(buf_t *buf, const uint8_t *mac, net_protocol_t protocol)
     // 调用buf_add_header()函数添加以太网包头
     buf_add_header(buf, sizeof(ether_hdr_t));
     ether_hdr_t *hdr = (ether_hdr_t *)buf->data;
-    
     // 填写目的MAC地址
-
+    memcpy(hdr->dst, mac, NET_MAC_LEN);
     // 填写源MAC地址，即本机的MAC地址
-
+    memcpy(hdr->src, net_if_mac, NET_MAC_LEN);
     // 填写协议类型 protocol
-
+    hdr->protocol16 = swap16(protocol);
     // 将添加了以太网包头的数据帧发送到驱动层
     driver_send(buf);
 }
